3759bd3e9bb02558d9139942873aaa8dd4e2025c
Victrid <weihau.chiang@gmail.com>
2021-06-26 02:45:37 +0800

    Add commandline help and file I/O.

--- OPGUtils/printer.py
+++ OPGUtils/printer.py
@@ -1,7 +1,9 @@
 from OPGUtils.common import Priority
-import importlib
+
 
 def printer(table, terminal_table):
+    lines = []
+
     def judge(item):
         if item == Priority.more:
             return ">"
@@ -11,6 +13,7 @@ def printer(table, terminal_table):
             return "="
         return ""
 
-    print("\t" + "\t".join(terminal_table))
+    lines.append("\t" + "\t".join(terminal_table))
     for line, body in zip(table, terminal_table):
-        print(body +"\t"+ "\t".join([judge(item) for item in line]))
+        lines.append(body + "\t" + "\t".join([judge(item) for item in line]))
+    return [line + "\n" for line in lines]
--- main.py
+++ main.py
@@ -1,19 +1,63 @@
 #!/bin/python
 
 # Written with Python 3.9.5
-
+import argparse
 import sys
-from BNFUtils import lex
+
 from BNFUtils.BNF_LALR_const import Symbol, Action, Goto, Reduce, GrammarSymbol, StateName
 from BNFUtils.LALR_parse import parse
+from BNFUtils.lex import lex
+from BNFUtils.lex import prepare
 from BNFUtils.static import static_analysis
+from OPGUtils.common import Priority
+from OPGUtils.construct import first_vt, last_vt
+from OPGUtils.filltable import fill_table
+from OPGUtils.printer import printer
+from OPGUtils.static import OPG_check, end_symbol
 
 
 def main():
-    lines = sys.stdin.readlines()
+    args = parse_arguments()
+
+    if not args.trace:
+        sys.tracebacklimit = 0
+
+    if args.input_file is None:
+        inputf = sys.stdin
+    else:
+        inputf = open(args.input_file, "r")
+    lines = inputf.readlines()
     t = "".join(lines)
-    lex_result = lex.prepare(lex.lex(t))
-    print(static_analysis(parse(lex_result, GrammarSymbol, StateName, Symbol, Reduce, Action, Goto)))
+
+    lex_result = prepare(lex(t))
+    grammar, terminal_table, symbol_table, init_symbol = static_analysis(
+        parse(lex_result, GrammarSymbol, StateName, Symbol, Reduce, Action, Goto))
+
+    grammar, terminal_table, symbol_table = end_symbol(grammar, symbol_table, terminal_table, init_symbol)
+    OPG_check(grammar)
+    firstvt = first_vt(grammar)
+    lastvt = last_vt(grammar)
+    table = [[Priority.undefined for _ in range(len(terminal_table))] for _ in range(len(terminal_table))]
+    fill_table(table, firstvt, lastvt, grammar)
+
+    lines = printer(table, terminal_table)
+    if args.output_file is None:
+        outputf = sys.stdout
+    else:
+        outputf = open(args.output_file, "w+")
+    outputf.writelines(lines)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Generate the OPG Table.')
+    parser.add_argument("-i", "--input-file", dest="input_file", default=None,
+                        help="Specify input file.")
+    parser.add_argument("-o", "--output-file", dest="output_file", default=None,
+                        help="Specify output file.")
+    parser.add_argument("--need-traceback", dest="trace", action="store_true", default=False,
+                        help="Show the traceback when error occurs.")
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
